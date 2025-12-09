f5e17a414e3837d3224ae563c5f3490a10fb0fad
Jan Malevskiy <janrule.gaming@gmail.com>
2025-12-09 12:52:44 -0500

    Main.java

--- StudentHandbookHelper/src/Main.java
+++ StudentHandbookHelper/src/Main.java
@@ -1,7 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            File Memory = new File("Memory.txt");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(Memory));
+            BufferedReader br = new BufferedReader(new FileReader(Memory));
+            String line;
+            if (!Memory.exists()) {
+                Memory.createNewFile();
+            }
 
+            if (br.readLine() == null) {
+                bw.write("test\n");
+                bw.close();
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
