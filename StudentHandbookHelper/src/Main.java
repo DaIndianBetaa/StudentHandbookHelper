b76aea695593f7f558fa699f841cf6fcc496939d
Jan Malevskiy <janrule.gaming@gmail.com>
2025-12-09 14:57:47 -0500

    Main.java

--- StudentHandbookHelper/src/Main.java
+++ StudentHandbookHelper/src/Main.java
@@ -2,22 +2,6 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            File Memory = new File("Memory.txt");
-            BufferedWriter bw = new BufferedWriter(new FileWriter(Memory));
-            BufferedReader br = new BufferedReader(new FileReader(Memory));
-            String line;
-            if (!Memory.exists()) {
-                Memory.createNewFile();
-            }
 
-            if (br.readLine() == null) {
-                bw.write("test\n");
-                bw.close();
-            }
-        }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
