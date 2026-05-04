3a04ef51274440bea175a7b3ed49be9fb0d9172e
Jan Malevskiy <janrule.gaming@gmail.com>
2026-05-04 10:11:15 -0400

    Create VolunteerProject.java

---
+++ GUI/VolunteerProject.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.time.LocalDate;
+
+
+class VolunteerProject{
+    private String Title;
+    private String Description;
+    private LocalDate Date;
+    private Float Hours;
+
+    public VolunteerProject(String _title, String _description, LocalDate _date, Float _hours){
+        Title = _title;
+        Description = _description;
+        Date = _date;
+        Hours = _hours;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public Float getHours() {
+        return Hours;
+    }
+
+    public void setHours(Float hours) { Hours = hours; }
+
+    public LocalDate getDate() {
+        return Date;
+    }
+
+    public void setDate(LocalDate date) {
+        Date = date;
+    }
+
+    @Override
+    public String toString() {
+        return  Title + '\n' +
+                Hours + " hours \t" +
+                Date + "\n" +
+                "Description: " + Description + "\n"+"----- \n";
+    }
+}
+
+
